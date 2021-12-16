c429d0f213d1f87bad54bd87e35931eda09b78f0
Carbon225 <carbon225@protonmail.com>
2021-12-16 01:13:06 +0100

    garbage works

--- project1/example.py
+++ project1/example.py
@@ -1,18 +1,22 @@
 from data import runtests
-from queue import PriorityQueue
 from math import inf
+from collections import deque
 
 
 class ResNet:
-    def __init__(self, V, E):
+    def __init__(self, V, E, max_flow):
+        V += 1
+
         self.connections = [set() for _ in range(V)]
         self.edge_losses = dict()
         self.current_flow = dict()
 
-        for (u, v), losses in E:
-            u -= 1
-            v -= 1
+        self.connections[0].add(1)
+        self.edge_losses[(0, 1)] = [0] * max_flow
+        self.current_flow[(0, 1)] = 0
+        self.current_flow[(1, 0)] = 0
 
+        for (u, v), losses in E:
             self.connections[u].add(v)
             self.connections[v].add(u)
             self.edge_losses[(u, v)] = losses
@@ -30,28 +34,38 @@ class ResNet:
                 losses += self.edge_losses[(u, v)][flow - 1]
         return losses
 
-    def connections_and_current_weights_for_vertex(self, u):
+    def residual_flows_and_costs(self, u):
         for v in self.connections[u]:
             flow = self.current_flow[(u, v)]
+            losses = self.edge_losses.get((u, v), None)
 
-            if flow == 0 and (u, v) in self.edge_losses:
-                yield v, self.edge_losses[(u, v)][0]
-            elif 0 < flow:
-                losses = self.edge_losses[(u, v)]
-                if flow < len(losses):
-                    old_w = losses[flow - 1]
-                    new_w = losses[flow]
-                    delta_w = new_w - old_w
-                    yield v, delta_w
-            # elif flow < 1:
-            #     losses = self.edge_losses[(v, u)]
-            #     old_w = losses[-flow - 1]
-            #     new_w = losses[-flow - 2]
-            #     delta_w = new_w - old_w
-            #     yield v, -delta_w
-            # elif flow < 0:
-            #     losses = self.edge_losses[(v, u)]
-            #     yield v, -losses[0]
+            if flow == 0 and losses is not None:
+                capacity = len(losses)
+                yield v, capacity - flow, losses[0]
+            elif 0 < flow < len(losses):
+                capacity = len(losses)
+                old_w = losses[flow - 1]
+                new_w = losses[flow]
+                delta_w = new_w - old_w
+                yield v, capacity - flow, delta_w
+            elif flow < -1:
+                losses = self.edge_losses[(v, u)]
+                old_w = losses[-flow - 1]
+                new_w = losses[-flow - 2]
+                delta_w = new_w - old_w
+                yield v, -flow, delta_w
+            elif flow < 0:
+                losses = self.edge_losses[(v, u)]
+                yield v, -flow, -losses[0]
+
+    def cost_edges(self):
+        for u in range(len(self)):
+            for v, flow, w in self.residual_flows_and_costs(u):
+                yield u, v, w
+
+    def residual_flows(self, u):
+        for v, flow, w in self.residual_flows_and_costs(u):
+            yield v, flow
 
 
 def backtrack_path(parents, s):
@@ -60,106 +74,117 @@ def backtrack_path(parents, s):
     return backtrack_path(parents, parents[s]) + [s]
 
 
-def dijkstra(G, s, t):
-    V = len(G)
-    Q = PriorityQueue()
-    d = [inf] * V
-    visited = [False] * V
-    parents = [None] * V
+def bellman_ford(E, n, s):
+    parent = [None] * n
+    d = [inf] * n
     d[s] = 0
-    Q.put((d[s], s))
 
-    while not Q.empty():
-        _, u = Q.get()
-        if visited[u]:
-            continue
-        visited[u] = True
+    def relax(u, v, w):
+        if d[u] + w < d[v]:
+            d[v] = d[u] + w
+            parent[v] = u
 
-        for (v, w_uv) in G.connections_and_current_weights_for_vertex(u):
-            if d[u] + w_uv < d[v]:
-                d[v] = d[u] + w_uv
-                Q.put((d[v], v))
-                parents[v] = u
+    for i in range(n - 1):
+        for (u, v, w) in E:
+            relax(u, v, w)
 
-    return backtrack_path(parents, t)
+    for (u, v, w) in E:
+        if d[u] + w < d[v]:
+            d[v] = d[u] + v
+            parent[v] = u
+            C = v
+            for _ in range(n):
+                C = parent[C]
+            cycle = []
+            v = C
+            while True:
+                cycle.append(v)
+                if v == C and len(cycle) > 1:
+                    break
+                v = parent[v]
+            cycle.reverse()
+            return cycle
+
+    return None
 
 
-def find_expansion(G, s, t):
-    return dijkstra(G, s, t)
+def find_negative_cycle(G, s):
+    return bellman_ford(list(G.cost_edges()), len(G), s)
 
 
-def process_expansion(G, path):
+def bfs(G, s, t):
+    V = len(G)
+    visited = [False] * V
+    parent = [None] * V
+    Q = deque()
+
+    visited[s] = True
+    Q.append(s)
+
+    while len(Q) > 0:
+        u = Q.popleft()
+        for v, w in G.residual_flows(u):
+            if w > 0 and not visited[v]:
+                visited[v] = True
+                parent[v] = u
+                Q.append(v)
+
+    if parent[t] is None:
+        return None
+
+    return backtrack_path(parent, t)
+
+
+def find_path_flow(G, path):
+    flow = inf
     u = path[0]
     for v in path[1:]:
-        G.current_flow[(u, v)] += 1
-        G.current_flow[(v, u)] -= 1
+        flow = min(flow, len(G.edge_losses[(u, v)]) - G.current_flow[(u, v)])
+        u = v
+    return flow
+
+
+def run_flow(G, path, flow):
+    u = path[0]
+    for v in path[1:]:
+        G.current_flow[(u, v)] += flow
+        G.current_flow[(v, u)] -= flow
         u = v
 
 
-def find_min_losses(G, target_division_count):
-    s = 0
-    t = len(G) - 1
+def ford_fulkerson(G, s, t):
+    max_flow = 0
 
-    division_count = 0
-    while division_count < target_division_count:
-        path = find_expansion(G, s, t)
+    while True:
+        path = bfs(G, s, t)
         if path is None:
-            print("rip")
             break
-        process_expansion(G, path)
-        division_count += 1
+        flow = find_path_flow(G, path)
+        run_flow(G, path, flow)
+        max_flow += flow
+
+    return max_flow
+
+
+def find_max_flow(G, s, t):
+    return ford_fulkerson(G, s, t)
+
+
+def min_cost_max_flow(G, s, t):
+    find_max_flow(G, s, t)
+
+    while True:
+        cycle = find_negative_cycle(G, s + 1)
+        if cycle is None:
+            break
+        run_flow(G, cycle, 1)
 
     return G.get_losses()
 
 
 def my_solve(V, k, edges):
-    return find_min_losses(ResNet(V, edges), k)
+    G = ResNet(V, edges, k)
+    return min_cost_max_flow(G, 0, len(G) - 1)
 
 
 runtests(my_solve)
-
-
-def tests():
-    # Dijkstra
-
-    G = ResNet(4, [
-        ((1, 2), [1, 10, 50, 400]),
-        ((1, 3), [3, 7, 12]),
-        ((2, 4), [1, 2, 3]),
-        ((3, 4), [1, 2, 3]),
-    ])
-
-    if dijkstra(G, 0, 3) == [0, 1, 3]:
-        print("Dijkstra PASS")
-    else:
-        print("Dijkstra ERROR:" + str(dijkstra(G, 0, 3)))
-
-    G.current_flow[(0, 1)] += 1
-    G.current_flow[(1, 0)] -= 1
-    G.current_flow[(1, 3)] += 1
-    G.current_flow[(3, 1)] -= 1
-
-    if dijkstra(G, 0, 3) == [0, 2, 3]:
-        print("Dijkstra PASS")
-    else:
-        print("Dijkstra ERROR:" + str(dijkstra(G, 0, 3)))
-
-    G.current_flow[(0, 2)] += 1
-    G.current_flow[(2, 0)] -= 1
-    G.current_flow[(2, 3)] += 1
-    G.current_flow[(3, 2)] -= 1
-
-    if dijkstra(G, 0, 3) == [0, 2, 3]:
-        print("Dijkstra PASS")
-    else:
-        print("Dijkstra ERROR:" + str(dijkstra(G, 0, 3)))
-
-    G.current_flow[(0, 2)] += 1
-    G.current_flow[(2, 0)] -= 1
-    G.current_flow[(2, 3)] += 1
-    G.current_flow[(3, 2)] -= 1
-
-    print(G.get_losses())
-
-
-# tests()
