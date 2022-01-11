55273fc4e9b4bccb6b8c2f7797161eb30514344f
Carbon225 <carbon225@protonmail.com>
2022-01-11 17:39:41 +0100

    check lex

---
+++ lab4/check_lex.py
@@ -0,0 +1,17 @@
+def check_lex_bfs(G, vs):
+    n = len(G)
+    pi = [None] * n
+    for i, v in enumerate(vs):
+        pi[v] = i
+
+    for i in range(n - 1):
+        for j in range(i + 1, n - 1):
+            Ni = G[vs[i]].out
+            Nj = G[vs[j]].out
+
+            verts = [pi[v] for v in Nj - Ni if pi[v] < i]
+            if verts:
+                viable = [pi[v] for v in Ni - Nj]
+                if not viable or min(verts) <= min(viable):
+                    return False
+    return True
